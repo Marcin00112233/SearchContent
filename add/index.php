4bef7ec10eba64d45756483f11afbb98e84de2ab
Marcin <mbartlinski@o2.pl>
2018-03-25 15:29:01 +0200

    Revert "wyczyszczenie wszystkich danych"
    This reverts commit f3879aae2dff72216e823a03f472627dc6cf09cd.

---
+++ add/index.php
@@ -0,0 +1,13 @@
+<?php /*moduł dodawania stron internetowych do wyszukiwarki */ ?>
+
+<html>
+<head>
+</head>
+<body>
+<form>
+<p>Dodaj strone internetowa</p>
+<p> url<input type="text"></input></p>
+<input type="submit" value="dodaj"></input>
+</form>
+</body>
+</html>
