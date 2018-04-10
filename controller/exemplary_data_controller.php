e40cb90eaeb2e654a0c6482ed569925ccebe66fe
Marcin <mbartlinski@o2.pl>
2018-04-10 22:49:36 +0200

    I added method show_form()

--- controller/exemplary_data_controller.php
+++ controller/exemplary_data_controller.php
@@ -2,6 +2,10 @@
 include "controller.php";
 
 class exemplary_data_controller extends controller{
+	public function show_form(){
+		$view = $this->loadView('exemplary_data');
+		$view->load_url_form();
+	}
 	
 }
 ?>
