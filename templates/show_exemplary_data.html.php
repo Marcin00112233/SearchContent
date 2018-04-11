1c67cabfa28ac6c298e57ecc23a31a9eca4a599e
Marcin <mbartlinski@o2.pl>
2018-04-11 03:56:44 +0200

    I added the form an addition url

---
+++ templates/show_exemplary_data.html.php
@@ -0,0 +1,10 @@
+<?php include 'header.html.php'; ?>
+  <form action="index.php?module=admin&proc=proc_add" method="post" >
+  <H1>SearchContent</H1>
+  <p>Wprowadź adres katalogu do pobrania treści</p>
+  <input type="text" name=search>
+  <input type="submit" value="search">
+  </form>
+  
+<?php include 'footer.html.php'; ?>
+?>
