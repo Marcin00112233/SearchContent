40c6a24c955d006aafed116b924a2fba2e64223e
Marcin <mbartlinski@o2.pl>
2018-04-22 20:14:13 +0200

    I created the form of adding data to the database

--- templates/show_exemplary_data.html.php
+++ templates/show_exemplary_data.html.php
@@ -1,5 +1,5 @@
 <?php include 'header.html.php'; ?>
-  <form action="index.php?module=admin&proc=proc_add" method="post" >
+<form action="index.php?module=admin&proc=proc_add" method="post" >
   <H1>SearchContent</H1>
   <p>Wprowadź adres katalogu do pobrania treści</p>
   <input type="text" name="add">
@@ -7,4 +7,3 @@
   </form>
   
 <?php include 'footer.html.php'; ?>
-?>
